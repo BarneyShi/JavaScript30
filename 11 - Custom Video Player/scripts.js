ded3922f5b2a61c9470587922c42d04196120e0b
BarneyShi <barneyshek@gmail.com>
2021-05-03 23:42:40 -0700

    Implement playbackrate & forward & backward & volume function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,6 @@
+const video = document.getElementsByTagName("video")[0];
+
 const play = () => {
-  const video = document.getElementsByTagName("video")[0];
   if (video.paused) {
     video.play();
   } else {
@@ -7,4 +8,27 @@ const play = () => {
   }
 };
 
+const setProgressBar = () => {
+  const progressBar = document.querySelector(".progress__filled");
+  const currentProgress = video.currentTime;
+  const length = video.duration;
+  const percentage = currentProgress / length;
+  progressBar.style.flexBasis = percentage * 100 + "%";
+};
 setInterval(setProgressBar, 500);
+
+const forward = () => {
+    video.currentTime = video.currentTime + 25;
+}
+
+const backward = () => {
+    video.currentTime = video.currentTime - 10;
+}
+
+const changePlayRate = event => {
+    video.playbackRate = parseFloat(event.target.value);
+}
+
+const changeVolume = event => {
+    video.volume = parseFloat(event.target.value);
+}
